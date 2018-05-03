ef1bf4695c9e57c7b24ac726fd4aa65daaa7265e
Yury Kozhenov <yurykozhenov@gmail.com>
2018-05-04 02:45:34 +0300

    Refactor css to jss, add toolbar

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,27 +1,57 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-
-import './App.css';
+import { withStyles } from 'material-ui/styles';
+import AppBar from 'material-ui/AppBar';
+import Toolbar from 'material-ui/Toolbar';
+import Typography from 'material-ui/Typography';
+import Button from 'material-ui/Button';
+import IconButton from 'material-ui/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
 
 import TodoListContainer from '../../containers/TodoListContainer';
 import Home from '../Home/Home';
 
-class App extends Component {
-  render() {
-    return (
-      <Router>
-        <div>
-          <nav>
-            <Link to="/">Home</Link>
-            <Link to="/todos">Todos</Link>
-          </nav>
+const styles = {
+  root: {
+    flexGrow: 1,
+  },
+  flex: {
+    flex: 1,
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20,
+  },
+};
 
-          <Route exact path="/" component={Home} />
-          <Route path="/todos" component={TodoListContainer} />
-        </div>
-      </Router>
-    );
-  }
-}
+const App = ({ classes }) => (
+  <Router>
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="Menu"
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography className={classes.flex} variant="title" color="inherit">
+            React Todo App
+          </Typography>
+          <Button component={Link} color="inherit" to="/">
+            Home
+          </Button>
+          <Button component={Link} color="inherit" to="/todos">
+            Todos
+          </Button>
+        </Toolbar>
+      </AppBar>
 
-export default App;
+      <Route exact path="/" component={Home} />
+      <Route path="/todos" component={TodoListContainer} />
+    </div>
+  </Router>
+);
+
+export default withStyles(styles)(App);
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,20 +1,48 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { withStyles } from 'material-ui/styles';
+
 import logo from '../../logo.svg';
 
-class Home extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+const styles = {
+  app: {
+    textAlign: 'center',
+  },
+  appLogo: {
+    animation: 'App-logo-spin infinite 20s linear',
+    height: 80,
+  },
+  appHeader: {
+    backgroundColor: '#222',
+    height: 150,
+    padding: 20,
+    color: 'white',
+  },
+  appTitle: {
+    fontSize: '1.5em',
+  },
+  appIntro: {
+    fontSize: 'large',
+  },
+  '@keyframes App-logo-spin': {
+    from: {
+      transform: 'rotate(0deg)',
+    },
+    to: {
+      transform: 'rotate(360deg)',
+    },
+  },
+};
 
-export default Home;
+const Home = ({ classes }) => (
+  <div className={classes.app}>
+    <header className={classes.appHeader}>
+      <img src={logo} className={classes.appLogo} alt="logo" />
+      <h1 className={classes.appTitle}>Welcome to React</h1>
+    </header>
+    <p className={classes.appIntro}>
+      To get started, edit <code>src/App.js</code> and save to reload.
+    </p>
+  </div>
+);
+
+export default withStyles(styles)(Home);
