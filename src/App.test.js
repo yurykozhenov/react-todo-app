5aed4e58b68baf50ac9d029f7f414959653482cf
Yury Kozhenov <yurykozhenov@gmail.com>
2018-05-28 14:37:25 +0300

    Fix tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { MemoryRouter } from 'react-router-dom';
+import { createStore } from 'redux';
 
 import App from './App';
 
+const store = createStore(
+  (state = { auth: { isAuthenticated: false } }) => state,
+);
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(
+    <Provider store={store}>
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>
+    </Provider>,
+    div,
+  );
   ReactDOM.unmountComponentAtNode(div);
 });
